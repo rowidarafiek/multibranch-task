0cfc62c8f1aa82b7eb45b9a8c18188e9556e9f2e
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-10 21:43:40 +0200

    update push to git hub function

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -6,17 +6,26 @@ stage('Push Changes to GitHub') {
                 usernameVariable: 'GIT_USER',
                 passwordVariable: 'GIT_PASS'
             )]) {
-                sh """
+                // Clean any uncommitted changes and build artifacts
+                sh '''
                     git reset --hard
                     git clean -fdx
-                    git fetch origin
+                '''
+
+                // Checkout branch or create if missing
+                sh """
+                    git fetch origin ${BRANCH_NAME}
                     git checkout ${BRANCH_NAME} || git checkout -b ${BRANCH_NAME}
                     git config user.name "${GIT_USER}"
                     git config user.email "jenkins@local"
-                    git add deployment.yaml
+                    git add .
                     git diff --cached --quiet || git commit -m "${COMMIT_MESSAGE}"
-                    git push https://${GIT_USER}:${GIT_PASS}@github.com/rowidarafiek/multibranch-task.git ${BRANCH_NAME}
                 """
+
+                // Push using secure string
+                sh '''
+                    git push https://$GIT_USER:$GIT_PASS@github.com/rowidarafiek/multibranch-task.git ${BRANCH_NAME}
+                '''
             }
         }
     }
