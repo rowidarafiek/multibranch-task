f5667d7aeb90bf0e2de4c4a774da2e21c96aba85
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-10 23:05:08 +0200

    update push docker image function

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,11 +1,9 @@
-// vars/pushDocker.groovy
-def call(String imageName, String imageTag, String dockerCreds) {
-    echo "Pushing Docker image ${imageName}:${imageTag}"
-    withCredentials([usernamePassword(credentialsId: dockerCreds, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh '''
-            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-            docker push ${imageName}:${imageTag}
-        '''
+def call(String imageName, String imageTag, String credentialsId) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+        echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+        docker push ${imageName}:${imageTag}
+        """
     }
 }
 
