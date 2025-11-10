0e0fed52fab9dc5b41b505e2c01c33adcbd76776
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-10 20:52:37 +0200

    updateDeployment function

--- vars/updateDeploymentYaml.groovy
+++ vars/updateDeploymentYaml.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String tag) {
     echo "Updating deployment.yaml with ${imageName}:${tag}"
     sh """
-        sed -i 's|image: .*|image: ${imageName}:${tag}|' k8s/deployment.yaml
+        sed -i 's|image: .*|image: ${imageName}:${tag}|' deployment.yaml
     """
 }
 
