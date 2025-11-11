d1416df2aa2c7229265b75a70de53df57ea16ec0
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-11 16:52:46 +0200

    update build app function

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,14 @@
 def call() {
-       echo "Building application..."
-            sh 'mvn clean package -DskipTests'
-        
+          echo "Building Java app..."
+    sh 'mvn clean package -DskipTests'
+
+    echo "Verifying JAR..."
+    sh '''
+        if [ ! -f target/demo-0.0.1-SNAPSHOT.jar ]; then
+            echo "JAR not found after build."
+            exit 1
+        fi
+        ls -lh target/
+    '''
 }
 
