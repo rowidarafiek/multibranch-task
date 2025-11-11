ac4615b9d9ef18d988153edf15d782899eee05c3
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-11 16:32:31 +0200

    update build docker image and push to git hub

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,6 @@
-def call() {
-    echo "Building Docker image ${env.IMAGE_NAME}:${env.IMAGE_TAG}"
-    sh "docker build -t ${env.IMAGE_NAME}:${env.IMAGE_TAG} ."
+def call() {    echo "Building Docker image: ${DOCKER_IMAGE}:${DOCKER_TAG}"
+    sh """
+        docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .
+        docker tag ${DOCKER_IMAGE}:${DOCKER_TAG} ${DOCKER_IMAGE}:latest
+    """
 }
-
--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -1,17 +1,22 @@
-def call(String branchName, String commitMessage) {
-    echo "Pushing changes to GitHub on branch ${branchName}"
-    withCredentials([usernamePassword(
-        credentialsId: 'github-cred',
-        usernameVariable: 'GIT_USER',
-        passwordVariable: 'GIT_PASS'
-    )]) {
-        sh """
-            git config user.name "${GIT_USER}"
-            git config user.email "jenkins@local"
-            git add .
-            git commit -m "${commitMessage}" || echo "No changes to commit"
-            git push https://${GIT_USER}:${GIT_PASS}@github.com/rowidarafiek/jenkins.git ${branchName}
-        """
+def call(){
+} withCredentials([usernamePassword(credentialsId: 'github', 
+                                      usernameVariable: 'GIT_USER', 
+                                      passwordVariable: 'GIT_PASS')]) {
+        sh '''
+            git config user.name "rowidarafiek"
+            git config user.email "rowidarafiek@domain.com"
+            
+            BRANCH=${BRANCH_NAME:-dev}
+            git checkout -B $BRANCH
+            
+            git add ${DEPLOYMENT_FILE}
+            
+            if git diff --staged --quiet; then
+                echo "No changes to commit"
+            else
+                git commit -m "Automated update from Jenkins build #${BUILD_NUMBER}"
+                git push https://${GIT_USER}:${GIT_PASS}@github.com/rowidarafiek/Argocd.git HEAD:$BRANCH
+                echo "Successfully pushed changes to GitHub"
+            fi
+        '''
     }
-}
-
